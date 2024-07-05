59736ddd95d12c818e58ecbba62548dd1fcff778
Joao Victor <euvictorj@gmail.com>
2024-07-05 16:27:46 -0300

    padrao background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
       items-center justify-center absolute top-0 left-0">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center
-      justify-center dark:bg-black bg-white
+      justify-center dark:bg-black-100 bg-white
       [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       
